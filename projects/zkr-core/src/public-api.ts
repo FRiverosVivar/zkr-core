26a6e739cbc14cba59dca53896d5245952a1071f
friveros98 <francoriverosvivar@gmail.com>
2023-06-12 16:55:33 -0400

    no message

---
+++ projects/zkr-core/src/public-api.ts
@@ -0,0 +1,18 @@
+/*
+ * Public API Surface of zkr-core
+ */
+
+export * from './lib/zkr-core.module';
+export * from './lib/core/components/table/table.component';
+export * from './lib/core/utils/pipes/filterObjectArrayByKeys.pipe';
+export * from './lib/core/model/zkr-core.model';
+export * from './lib/core/model/zkr-core-login.model';
+export * from './lib/core/model/zkr-core-country.model';
+export * from './lib/core/model/zkr-core-progress-bar.model';
+export * from './lib/core/ngx-progressbar-wrapper/component/zkr-progress-bar.component';
+export * from './lib/core/services/auth/auth.types';
+export * from './lib/core/services/auth/auth.service';
+export * from './lib/core/services/http/http-base.service';
+export * from './lib/core/services/http/http.types';
+export * from './lib/core/services/http-auth/http-auth.service';
+export * from './lib/core/services/local-storage/local-storage.service';
